2b0fcf200654697bdaf2455447849f6a47f4fc36
worthlesspenny7 <worthlesspenny7@worthlesspenny7-VirtualBox.(none)>
2015-12-29 23:24:09 -0500

    added for loop to paragraph

--- application.py
+++ application.py
@@ -1,10 +1,14 @@
 from flask import Flask, render_template
 
+
 application = app = Flask(__name__)
 
 @app.route('/')
 def homepage():
-	return render_template("index.html")
+
+	title = "Yeah Buddy, I've got a title!."
+	paragraph = ["does this work without brackets?", "I hope so, it should be a list now", "if it's not a list, it will be an ugly paragraph."]
+	return render_template("index.html", title=title, paragraph=paragraph)
 	
 if __name__ == "__main__":
 	app.run()
